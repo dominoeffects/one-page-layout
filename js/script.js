934872a3ca8d8d8848323dfa800e6ea704b6741e
Dominique Maez <Domino@Dominiques-MacBook-Pro-2.local>
2019-05-02 17:58:14 -0700

    part one

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".box").mouseover(function(){
+      $(".box").css('width', '400px');
+    });
+  });
+
+  $(document).ready(function(){
+    $(".box").mouseover(function(){
+      $(".box").css('margin', '10px');
+    });
+  });
